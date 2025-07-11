96350f8053d6ab0e96a099496b2f37a8f562a745
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-11 11:03:34 +0300

    Подготовлена структура приложения

---
+++ TicTacToeApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeApp.Services;
+
+namespace TicTacToeApp.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private MainService _mainService;
+
+        public MainViewModel()
+        {
+            _mainService = new MainService();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
